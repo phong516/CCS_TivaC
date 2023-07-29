875f0501178d53bf90e215d1d428508dbf869749
phong516 <tiphong113@gmail.com>
2023-07-29 21:54:33 +0700

    nothing

--- pwm_demo/gpio.c
+++ pwm_demo/gpio.c
@@ -20,3 +20,10 @@ void configGPIOOutput(GPIOA_Type *gpioPortX, uint8_t pin, uint8_t IOType){
     gpioPortX->PUR |= (1U << pin); //enable pull-up resistor
     gpioPortX->PCTL &= ~(0b1111U << (pin * 4)); // clear the bits of alternate pin config
 }
+
+void configGPIOInterrupt(GPIOA_Type *gpioPortX, uint8_t pin, uint8_t edgeType){
+    gpioPortX->IS &= ~(1U << pin); //set pin as edge sensitive
+    gpioPortX->IBE &= ~(1U << pin); //set pin as single edge sensitive
+    gpioPortX->IEV &= ~(1U << pin); //set pin as falling edge sensitive
+    gpioPortX->IM |= (1U << pin); //enable interrupt for pin
+}
--- pwm_demo/gpio.h
+++ pwm_demo/gpio.h
@@ -15,6 +15,22 @@
 
 #define _GPIOAFSEL_OFFSET   0x420
 
+/**
+ * @brief Config GPIO pin to be output or input
+ * 
+ * @param gpioPortX GPIOA to GPIOF or GPIOA_AHB to GPIOF_AHB
+ * @param pin   0 to 7
+ * @param IOType 0: input, 1: output
+ */
 void configGPIOOutput(GPIOA_Type *gpioPortX, uint8_t pin, uint8_t IOType);
+
+/**
+ * @brief Config GPIO pin interrupt edge type
+ * 
+ * @param gpioPortX GPIOA to GPIOF or GPIOA_AHB to GPIOF_AHB
+ * @param pin  0 to 7
+ * @param edgeType 
+ */
+void configGPIOInterrupt(GPIOA_Type *gpioPortX, uint8_t pin, uint8_t edgeType);
 
 #endif /* GPIO_H_ */
