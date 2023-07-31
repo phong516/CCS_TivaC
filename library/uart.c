b2030a95daad7f625b90126355d375b30576457e
phong516 <tiphong113@gmail.com>
2023-07-31 23:21:31 +0700

    complete enableUARTModule function

---
+++ library/uart.c
@@ -0,0 +1,69 @@
+#include "uart.h"
+
+void enableUARTModule(uint8_t uartModule, uint8_t port, uint8_t txPin, uint8_t rxPin){
+    SYSCTL->RCGCUART |= (1U << uartModule);
+    SYSCTL->RCGCGPIO |= (1U << port);
+    GPIOA_Type *gpioPort;
+    UART0_Type *uart;
+    switch(port){
+        case 0: 
+            gpioPort = GPIOA;
+            break;
+        case 1:
+            gpioPort = GPIOB;
+            break;
+        case 2:
+            gpioPort = GPIOC;
+            break;
+        case 3:
+            gpioPort = GPIOD;
+            break;
+        case 4:
+            gpioPort = GPIOE;
+            break;
+        case 5:
+            gpioPort = GPIOF;
+            break;
+        default:
+            return;
+    }
+
+    switch(uartModule){
+        case 0: 
+            uart = UART0;
+            break;
+        case 1:
+            uart = UART1;
+            break;
+        case 2:
+            uart = UART2;
+            break;
+        case 3:
+            uart = UART3;
+            break;
+        case 4:
+            uart = UART4;
+            break;
+        case 5:
+            uart = UART5;
+            break;
+        case 6:
+            uart = UART6;
+            break;
+        case 7:
+            uart = UART7;
+            break;
+        default:
+            return;
+    }
+    
+    alternatePinConfig(gpioPort, txPin, 0x1);
+    alternatePinConfig(gpioPort, rxPin, 0x1);
+    uart->CTL &= ~(1U << 0);
+    uart->IBRD = 10U;
+    uart->FBRD = 54U;
+    uart->LCRH = 0x60;
+    uart->CC &= ~(0b1111U);
+    uart->CTL |= (1U << 0);
+}
+
---
+++ library/uart.h
@@ -0,0 +1,17 @@
+/*
+ * interrupt.h
+ *
+ *  Created on: Jul 31, 2023
+ *      Author: Phong
+ */
+
+#ifndef UART_H_
+#define UART_H_
+
+#include "TM4C123GH6PM.h"
+#include "gpio.h"
+
+
+
+
+#endif /* UART_H_ */
